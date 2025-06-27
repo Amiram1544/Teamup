00414b436241fccc8698eb1e19cb49541e92f418
Amiram1544 <amiram1544@gmail.com>
2025-06-27 21:45:38 +0330

    Progress in Login page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'core/index.html')
+
+def login(request):
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, usename=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request,, 'Invalid username or password')
+            
+    context = {
+        page: 'page',
+    }        
+    return render(request, 'core/login.html', context)
