b0609da98ff42119d4da8c3edbfd907ba43920be
Amiram1544 <amiram1544@gmail.com>
2025-06-28 17:03:21 +0330

    Add email field for registeration

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class UserForm(ModelForm):
     class Meta:
+        email = forms.EmailField(required=True)
+        
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
