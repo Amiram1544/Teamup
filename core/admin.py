322ec994af794e6ddc9ece402dd8932bccdf4857
Amiram1544 <amiram1544@gmail.com>
2025-06-28 21:53:27 +0330

    Feat: Initial the Rooms model & Edit in index.html

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Topics
+from core.models import Topics, Rooms
 
 # Register your models here.
 
 admin.site.register(Topics)
+admin.site.register(Rooms)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,4 +11,20 @@ class Topics(models.Model):
         verbose_name_plural = 'Topics'
         
     def __str__(self):   
+        return self.name
+    
+class Rooms(models.Model):
+    
+    topics = models.ForeignKey(Topics, verbose_name=("topic"), on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, blank=True, related_name='participants')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Rooms'
+        ordering = ['-updated', '-created']
+        
+    def __str__(self):
         return self.name
