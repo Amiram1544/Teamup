919ba69e63d47ad0c6d8a95eb300b6d492d2f048
Amiram1544 <amiram1544@gmail.com>
2025-06-29 21:27:40 +0330

    Feat: Initial the models & Views for messages.

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import Rooms
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Messages(models.Model):
+    
+    rooms = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated =  models.DateTimeField(auto_now=True)
--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from core.models import Rooms
+from django.contrib import messages
 
 # Create your views here.
+
+@login_required(login_url='login')
+def room(request,pk):
+    
+    rooms = Rooms.objects.all(id=pk)
+    participants = rooms.objects.all()
+    
+    if not room:
+        messages.warning(request, 'Room not found')
+        return redirect('home')
+    
+    
+    context = {
+        'rooms': rooms,
+        'participants' : participants,
+    }
+    return render(request, 'chatrooms/room.html', context)
