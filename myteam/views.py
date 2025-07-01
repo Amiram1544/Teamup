562ab503803f71dd9972e62f05655a6687fe1dc7
Amiram1544 <amiram1544@gmail.com>
2025-07-02 03:27:53 +0330

    Refactor: Complete the views.

--- myteam/views.py
+++ myteam/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Teams, TeamMessages
+from django.contrib import messages
 
 # Create your views here.
 
@@ -21,10 +22,37 @@ def mainpage(request):
 def team_chat(request, team_id):
     
     team_page = Teams.objects.get(id=team_id)
+    team_members = team_page.members.all()
     team_messages = TeamMessages.objects.filter(team=team_page).order_by('created')
+    parent_id = request.POST.get('parent_id')
     
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            team_message = request.POST.get('team_messages', '').strip()
+            parent_message = None
+            if parent_id:
+                try:
+                    parent_message = TeamMessages.objects.get(id=parent_id)
+                except TeamMessages.DoesNotExist:
+                    parent_message = None
+                    
+            if team_message:
+                
+                TeamMessages.objects.create( 
+                    user = request.user,
+                    team = team_page,
+                    body = team_message,
+                    parent = parent_message,  
+                )
+                messages.success(request, 'Sent')
+                return redirect('myteam:team_chat', team_id=team_id)
+            else:
+                messages.error(request, 'Message cannot be empty. or an error occurred.')
+            
+            
     context = {
         'team_page': team_page,
         'team_messages': team_messages,
+        'team_members': team_members,
     }
     return render(request, 'myteam/team_chat.html', context)
