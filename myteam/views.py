c612d84e44fbe2cdb8a66506d519a493a02b44c7
Amiram1544 <amiram1544@gmail.com>
2025-07-01 23:31:00 +0330

    Feat: Teams display on page & Team members count.

--- myteam/views.py
+++ myteam/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Teams, TeamMessages
 
 # Create your views here.
 
 @login_required(login_url='login')
 def mainpage(request):
     
-    
-    return render(request, 'myteam/mainpage.html')
+    teams = Teams.objects.filter(members = request.user)
+    for team in teams:
+        team.first_members = team.members.all()[0:3]
+        team.other_members_count = team.members.count() - 3
+           
+    context = {
+        'teams': teams,
+    }
+    return render(request, 'myteam/mainpage.html', context)
