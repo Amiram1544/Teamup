368ad843f636ec84a3d1ba50064ea5a38a025c3a
Amiram1544 <amiram1544@gmail.com>
2025-07-01 21:15:26 +0330

    Feat: Add the team models.

--- myteam/models.py
+++ myteam/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Teams(models.Model):
+    
+    #host = 
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = 'Teams'
+    
+    def __str__(self):
+        return self.name
